3390d82cfaa0ed3712342c3140abf56bdd524dbb
Jerry <windfant@sina.com>
2020-04-24 00:00:40 +0800

    Init

---
+++ WFStream/WFStream.h
@@ -0,0 +1,19 @@
+//
+//  WFStream.h
+//  WFStream
+//
+//  Created by Jerry on 2020/4/23.
+//  Copyright © 2020 Jerry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WFStream.
+FOUNDATION_EXPORT double WFStreamVersionNumber;
+
+//! Project version string for WFStream.
+FOUNDATION_EXPORT const unsigned char WFStreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WFStream/PublicHeader.h>
+
+
